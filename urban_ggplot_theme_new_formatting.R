fa9893435a2a463b9d1a9a7ea3fcc32902d8420f
awunderground <awunderground@gmail.com>
2016-12-19 12:59:00 -0500

    Update formatting.

--- urban_ggplot_theme_new_formatting.R
+++ urban_ggplot_theme_new_formatting.R
@@ -1,45 +1,56 @@
-library('ggplot2')
-library('grid')
-library('RColorBrewer')
+library(ggplot2)
+library(grid)
+library(RColorBrewer)
 
 ########### Example plots #################
 
 ####Bar
 ##1 color
-#print(ggplot(mtcars, aes(factor(cyl))) + geom_bar() + coord_cartesian(ylim = c(0, 100))+ggtitle("Title"))
+#ggplot(mtcars, aes(factor(cyl))) + 
+#  geom_bar() + 
+#  coord_cartesian(ylim = c(0, 100)) +
+#  ggtitle("Title")
 
 ##3 colors
-#print(qplot(factor(cyl), data=mtcars, geom="bar", fill=factor(cyl))+ggtitle("Title"))
+#qplot(factor(cyl), data = mtcars, geom = "bar", fill = factor(cyl)) + labs(title = "Title")
 
 ##5 colors
-#print(ggplot(diamonds, aes(clarity, fill=cut)) + geom_bar() +ggtitle("Title") + coord_cartesian(ylim = c(0, 15000)))
-
+#ggplot(diamonds, aes(clarity, fill = cut)) + 
+#  geom_bar() +
+#  ggtitle("Title") + 
+#  coord_cartesian(ylim = c(0, 15000))
 
 ####Scatter
 ##3 colors
-#print(ggplot(mtcars, aes(wt, mpg))+geom_point(aes(colour = factor(cyl)))+ggtitle("Title"))
+#ggplot(mtcars, aes(wt, mpg)) + 
+#  geom_point(aes(colour = factor(cyl))) + 
+#  ggtitle("Title")
 
 ##9 colors
- # dsamp <- diamonds[sample(nrow(diamonds), 1000), ]
- # d <- qplot(carat, price, data=dsamp, colour=clarity, size = 3)
- # print(d+ggtitle("Title"))
+#dsamp <- diamonds[sample(nrow(diamonds), 1000), ]
+#qplot(carat, price, data = dsamp, colour = clarity, size = 3) +
+#  ggtitle("Title")
 
 ###Line
 ##3 colors
-# mtcars.long <- melt(mtcars, id = "mpg", measure = c("disp", "hp", "wt"))
-# print(ggplot(mtcars.long, aes(mpg, value, colour = variable)) + geom_line()+ggtitle("Title"))
+#library(reshape2)
+#ggplot(mtcars.long, aes(mpg, value, colour = variable)) +
+#  geom_line() +
+#  ggtitle("Title")
 
 ###Facet Grid
-# p <- ggplot(mtcars, aes(mpg, wt)) + geom_point() + ggtitle("Title")
-# print(p + facet_grid(vs ~ am, margins=TRUE))
+#ggplot(mtcars, aes(mpg, wt)) +
+#  geom_point() + 
+#  ggtitle("Title") +
+#  facet_grid(vs ~ am, margins = TRUE)
 
 ###Histogram
 ##22 colors
-# print(ggplot(mtcars) + scale_fill_manual(values = extendedPalette(22)) + geom_histogram(aes(factor(hp), fill=factor(hp))))
-
+#ggplot(mtcars) + 
+#  scale_fill_manual(values = extendedPalette(22)) + 
+#  geom_histogram(aes(factor(hp), fill = factor(hp)))
 
 ####################################
-
 
 #resize window to 650 px width
 #quartz.options(width = 8.33333333333333, height = 5.55555555555556, dpi = 72)
@@ -300,37 +311,37 @@ theme_new <- theme_update(panel.spacing.x = unit(0,"lines"),
 #############################
 
 #Redefine default discrete colors, up to 9 colors.
-scale_colour_discrete <- function(...) scale_colour_custom(..., palette="Set1")
-scale_fill_discrete <- function(...) scale_fill_custom(... , palette="Set1")
+scale_colour_discrete <- function(...) scale_colour_custom(..., palette = "Set1")
+scale_fill_discrete <- function(...) scale_fill_custom(... , palette = "Set1")
 
 #################### Functions to Define custom colours #####################
-divlist<-c("BrBG","PiYG","PRGn","PuOr","RdBu","RdGy","RdYlBu","RdYlGn","Spectral")
-quallist<-c("Accent","Dark2","Paired","Pastel1","Pastel2","Set1","Set2","Set3")
-seqlist<-c("Blues","BuGn","BuPu","GnBu","Greens","Greys","Oranges","OrRd",
+divlist <- c("BrBG","PiYG","PRGn","PuOr","RdBu","RdGy","RdYlBu","RdYlGn","Spectral")
+quallist <- c("Accent","Dark2","Paired","Pastel1","Pastel2","Set1","Set2","Set3")
+seqlist <- c("Blues","BuGn","BuPu","GnBu","Greens","Greys","Oranges","OrRd",
 "PuBu","PuBuGn","PuRd","Purples","RdPu","Reds","YlGn","YlGnBu","YlOrBr","YlOrRd")
 
 divnum <- rep(11, length(divlist))
 qualnum <- c( 8, 8, 12, 9, 8, 9, 8, 12)
 seqnum <- rep(9, length(seqlist))
 
-namelist<-c(divlist,quallist,seqlist)
+namelist <- c(divlist,quallist,seqlist)
 maxcolours <- c(divnum,qualnum,seqnum)
-catlist<-rep(c("div","qual","seq"),c(length(divlist),length(quallist),length(seqlist)))
+catlist <- rep(c("div","qual","seq"), c(length(divlist),length(quallist), length(seqlist)))
 
-custom.pal.info<-data.frame(maxcolours=maxcolours,category=catlist,row.names=namelist)
+custom.pal.info <- data.frame(maxcolours = maxcolours, category = catlist, row.names = namelist)
 
-custom.pal<-function(n,name){
- if(!(name %in% namelist)){
- stop(paste(name,"is not a valid palette name for custom.pal\n"))
+custom.pal <- function(n, name){
+ if (!(name %in% namelist)) {
+ stop(paste(name, "is not a valid palette name for custom.pal\n"))
  }
- if(n<3){
+ if (n < 3) {
  warning("minimal value for n is 3, returning requested palette with 3 different levels\n")
- return(custom.pal(3,name))
+ return(custom.pal(3, name))
  }
- if(n>maxcolours[which(name==namelist)]){
- warning(paste("n too large, allowed maximum for palette",name,"is",maxcolours[which(name==namelist)]),
+ if (n > maxcolours[which(name == namelist)]) {
+ warning(paste("n too large, allowed maximum for palette",name,"is", maxcolours[which(name == namelist)]),
 "\nReturning the palette you asked for with that many colours\n")
- return(custom.pal(maxcolours[which(name==namelist)],name))
+ return(custom.pal(maxcolours[which(name == namelist)], name))
  }
 
 c1 <- col2rgb("#1696d2")
@@ -349,46 +360,45 @@ c9 <- col2rgb("#761548")
 
 rgb(c(c1[1]),
   c(c1[2]),
-  c(c1[3]),maxColorValue=255),
+  c(c1[3]), maxColorValue = 255),
 rgb(c(c1[1],c2[1]),
   c(c1[2],c2[2]),
-  c(c1[3],c2[3]),maxColorValue=255),
+  c(c1[3],c2[3]), maxColorValue = 255),
 rgb(c(c1[1],c2[1],c3[1]),
   c(c1[2],c2[2],c3[2]),
-  c(c1[3],c2[3],c3[3]),maxColorValue=255),
+  c(c1[3],c2[3],c3[3]), maxColorValue = 255),
 rgb(c(c1[1],c2[1],c3[1],c4[1]),
   c(c1[2],c2[2],c3[2],c4[2]),
-  c(c1[3],c2[3],c3[3],c4[3]),maxColorValue=255),
+  c(c1[3],c2[3],c3[3],c4[3]), maxColorValue = 255),
 rgb(c(c1[1],c2[1],c3[1],c4[1],c5[1]),
   c(c1[2],c2[2],c3[2],c4[2],c5[2]),
-  c(c1[3],c2[3],c3[3],c4[3],c5[3]),maxColorValue=255),
+  c(c1[3],c2[3],c3[3],c4[3],c5[3]), maxColorValue = 255),
 rgb(c(c1[1],c2[1],c3[1],c4[1],c5[1],c6[1]),
   c(c1[2],c2[2],c3[2],c4[2],c5[2],c6[2]),
-  c(c1[3],c2[3],c3[3],c4[3],c5[3],c6[3]),maxColorValue=255),
+  c(c1[3],c2[3],c3[3],c4[3],c5[3],c6[3]), maxColorValue = 255),
 rgb(c(c1[1],c2[1],c3[1],c4[1],c5[1],c6[1],c7[1]),
   c(c1[2],c2[2],c3[2],c4[2],c5[2],c6[2],c7[2]),
-  c(c1[3],c2[3],c3[3],c4[3],c5[3],c6[3],c7[3]),maxColorValue=255),
+  c(c1[3],c2[3],c3[3],c4[3],c5[3],c6[3],c7[3]), maxColorValue = 255),
 rgb(c(c1[1],c2[1],c3[1],c4[1],c5[1],c6[1],c7[1],c8[1]),
   c(c1[2],c2[2],c3[2],c4[2],c5[2],c6[2],c7[2],c8[2]),
-  c(c1[3],c2[3],c3[3],c4[3],c5[3],c6[3],c7[3],c8[3]),maxColorValue=255),
+  c(c1[3],c2[3],c3[3],c4[3],c5[3],c6[3],c7[3],c8[3]), maxColorValue = 255),
 rgb(c(c1[1],c2[1],c3[1],c4[1],c5[1],c6[1],c7[1],c8[1],c9[1]),
   c(c1[2],c2[2],c3[2],c4[2],c5[2],c6[2],c7[2],c8[2],c9[2]),
-  c(c1[3],c2[3],c3[3],c4[3],c5[3],c6[3],c7[3],c8[3],c9[3]),maxColorValue=255),
+  c(c1[3],c2[3],c3[3],c4[3],c5[3],c6[3],c7[3],c8[3],c9[3]), maxColorValue = 255),
 ),
-  Set2 =  switch(n,
+  Set2 = switch(n,
 rgb(c(154),
   c(62),
-  c(37),maxColorValue=255),
+  c(37), maxColorValue = 255),
 rgb(c(154,21),
   c(62,107),
-  c(37,144),maxColorValue=255),
+  c(37,144), maxColorValue = 255),
 rgb(c(154,21,112),
   c(62,107,130),
-  c(37,144,89),maxColorValue=255)
+  c(37,144,89), maxColorValue = 255)
 )
 )
 }
-
 
 pal_name <- function(palette, type) {
 if (is.character(palette)) {
