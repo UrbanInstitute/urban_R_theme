0ddfa17e0b3b9445e4dfb623650c9fc20200fc0c
Kyle Ueyama <khueyama@gmail.com>
2018-02-18 10:41:25 -0500

    small edits to paths for testing

--- urban_theme_windows.R
+++ urban_theme_windows.R
@@ -1,2 +1,2 @@
 message('urban_theme_windows.R is deprecated. Consider using urban_theme.R instead')
-source('https://raw.githubusercontent.com/UrbanInstitute/urban_R_theme/urban_R_theme_revamp/urban_theme.R')
+source('https://raw.githubusercontent.com/UrbanInstitute/urban_R_theme/urban_R_wrapper/urban_theme.R')
